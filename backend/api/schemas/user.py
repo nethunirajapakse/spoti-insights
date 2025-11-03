b457bb3edac2278ae239c83dec00feba36c12bc3
Nethuni <nethunirajapakse@gmail.com>
2025-11-03 23:38:04 +0530

    additional changes in user model

--- backend/api/schemas/user.py
+++ backend/api/schemas/user.py
@@ -8,7 +8,7 @@ class UserBase(BaseModel):
     email: Optional[str]
 
 class UserCreate(UserBase):
-    refresh_token: str
+    spotify_refresh_token: str
 
 class UserResponse(UserBase):
     id: int
--- backend/models/user.py
+++ backend/models/user.py
@@ -2,11 +2,12 @@ from sqlalchemy import Column, Integer, String, TIMESTAMP
 from backend.database.connection import Base
 
 class User(Base):
-    __tablename__ = "users"  
+    __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
     spotify_id = Column(String, unique=True, index=True, nullable=False)
-    refresh_token = Column(String, nullable=False)
+    spotify_refresh_token = Column(String, nullable=False) 
+    app_refresh_token = Column(String)
     display_name = Column(String)
     email = Column(String)
     last_login = Column(TIMESTAMP(timezone=True))
