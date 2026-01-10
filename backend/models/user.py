841701b69e1c2f9cb7419b5c8bf7ac94c78e6eb2
Nethuni <nethunirajapakse@gmail.com>
2026-01-11 00:07:52 +0530

    Removed app-refresh-tocken from db

--- backend/models/user.py
+++ backend/models/user.py
@@ -7,7 +7,6 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     spotify_id = Column(String, unique=True, index=True, nullable=False)
     spotify_refresh_token = Column(String, nullable=False) 
-    app_refresh_token = Column(String)
     display_name = Column(String)
     email = Column(String)
     last_login = Column(TIMESTAMP(timezone=True))
