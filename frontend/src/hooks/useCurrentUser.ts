b9c5d7d62443a03e21e304d15adcc8d520b73826
Nethuni <nethunirajapakse@gmail.com>
2026-01-22 20:27:08 +0530

    Minor changes

--- frontend/src/hooks/useCurrentUser.ts
+++ frontend/src/hooks/useCurrentUser.ts
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import { getCurrentUser } from "@/api";
+import { getCurrentUser } from "@/api/auth";
 
 export const useCurrentUser = () => {
   return useQuery({
